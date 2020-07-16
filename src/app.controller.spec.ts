84bac362ed4f55e35c4176b0b00352175f1a5596
Mohamed Adel Ibrahim <ibrahim@riseup.co>
2020-07-17 01:26:17 +0200

    :white_check_mark: Add Number API Test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,9 +14,9 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('Add Numbers', () => {
+    it('should return sum of 2 numbers', () => {
+      expect(appController.addNumbers(1, 2)).toBe(3);
     });
   });
 });
